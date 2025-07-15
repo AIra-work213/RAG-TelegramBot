93926ad63ee2a48e806c46c522e826a90e6aa5bb
AIra <AIra.work213@gmail.com>
2025-07-15 14:45:38 +0700

    Исправлены мелкие ошибки

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 from telebot import TeleBot
-from env import Bot_token
+import os
 import requests
 
-bot = TeleBot(Bot_token)
+bot_token = os.getenv("BOT_TOKEN")
+if not bot_token:
+    raise ValueError("BOT_TOKEN environment variable is not set.")
+bot = TeleBot(bot_token)
 
 '''Основные команды'''
 @bot.message_handler(commands=["start"])
-async def start(message):
+def start(message):
     bot.send_message(message.chat.id, "Привет, я бот для поиска информации в векторной базе данных. /search для выполнения поиска и /add для добавления сайта с текстом")
 
 @bot.message_handler(commands=["help"])
-async def help(message):
+def help(message):
     bot.send_message(message.chat.id, "Я могу помочь вам найти информацию в векторной базе данных. Для этого вам нужно отправить мне сообщение с запросом.")
 
-'''Поиск'''
+'''Поиск в БД'''
 @bot.message_handler(commands=["search"])
-async def search(message):
+def search(message):
     bot.send_message(message.chat.id, "Введите ваш запрос:")
     bot.register_next_step_handler(message, search_handler)
 
-async def search_handler(message):
+def search_handler(message):
     query = message.text
-    response = requests.get(f"http://localhost:8000/search?query={query}")
-    bot.send_message(message.chat.id, response.json())
+    response = requests.get(f"http://chroma_server:8000/search?query={query}")
+    bot.send_message(message.chat.id, str(response.json()))
+
 
 '''Добавление ссылки пользователя в векторную БД'''
 @bot.message_handler(commands=["add"])
-async def add(message):
+def add(message):
     bot.send_message(message.chat.id, "Введите URL сайта:")
     bot.register_next_step_handler(message, add_handler)
 
-async def add_handler(message):
+def add_handler(message):
     url = message.text
-    response = requests.post(f"http://localhost:8000/add?url={url}")
-    bot.send_message(message.chat.id, response.json())
+    response = requests.post(f"http://chroma_server:8000/add?url={url}")
+    bot.send_message(message.chat.id, str(response.json()))
 
 bot.polling()
