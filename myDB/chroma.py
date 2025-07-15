4a4908a841734c514883198d0ba113b34810aab3
AIra <AIra.work213@gmail.com>
2025-07-15 20:45:57 +0700

    Добавлено логгирование и исправлен импорт langchain_huggingface

--- myDB/chroma.py
+++ myDB/chroma.py
@@ -1,20 +1,26 @@
 from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from bs4.filter import SoupStrainer
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logging.info("Успешный импорт")
 
 embed_func = HuggingFaceEmbeddings(
     model_name="ai-forever/sbert_large_nlu_ru",
     model_kwargs={"device": "cpu"}
 )
-strainer = SoupStrainer(class_=("post-title", "post-header", "post-content"))
+logging.info("Успешный импорт модели сбера")
+# Стрейнер для всех параграфов, заголовков и списков
+strainer = SoupStrainer(['article', 'main', 'section', 'div', 'p', 'h1', 'h2', 'h3', 'li'])
 loader = WebBaseLoader(
-    web_path=("https://www.wildberries.ru/catalog/tags/mercz-odezhda",),
+    web_path=("https://habr.com/ru/companies/tochka/articles/927386/","https://habr.com/ru/companies/onlinepatent/articles/927834/"),
     bs_kwargs={'parse_only': strainer}
 )
 docs = loader.load()
+print(f"Загружено документов: {len(docs)}")
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
@@ -24,7 +30,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 splitted_text = text_splitter.split_documents(documents=docs)
 
 vector_db = Chroma(
-    collection_name="Test",
+    collection_name="pages",
     embedding_function=embed_func,
     persist_directory="/Chroma_db",
 )
