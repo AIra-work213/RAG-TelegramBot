0d13480eadac6d5ec33c3e10a070cb2d414eafc2
AIra <AIra.work213@gmail.com>
2025-07-15 20:48:44 +0700

    Добавлено логгирование + фикс небольших ошибок + убрана llm, которая импортировалась по ошибке

--- myDB/query_llm.py
+++ myDB/query_llm.py
@@ -3,23 +3,31 @@
 '''
 
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from fastapi import FastAPI
 from uvicorn import run
 from chroma import add_to_db
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.info("Успешный импорт")
 
 """Часть для работы с векторной БД"""
+'''
 llm = HuggingFacePipeline.from_model_id(
     model_id="sberbank-ai/rugpt3large_based_on_gpt2",
     task="text-generation"
 )
+logging.info("Успешный импорт LLM сбера")
+'''
 embed_func = HuggingFaceEmbeddings(
     model_name="ai-forever/sbert_large_nlu_ru",
     model_kwargs={"device": "cpu"}
 )
+logging.info("Успешный импорт эмбед модели сбера")
 vector_db = Chroma(
-    collection_name='test',
+    collection_name='pages',
     persist_directory='/Chroma_db',
     embedding_function=embed_func
 )
@@ -29,7 +37,7 @@ def query_db(user_query):
     docs = vector_db.similarity_search_by_vector(user_vector, k=5)
     return [doc.page_content for doc in docs]
     
-
+logging.info("Подготовка к запуску сервера")
 """Серверная часть"""
 app = FastAPI()
 
